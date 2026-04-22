f652d48d056538ac7bd350bc78222e7c069329cd
Onni Hakala <onni@flaky.build>
2026-04-22 08:07:00 +0300

    chore: update NIF checksums for v0.6.1 + fix rebase-before-write in workflow

--- checksum-Elixir.ExUndercover.Nif.exs
+++ checksum-Elixir.ExUndercover.Nif.exs
@@ -1,6 +1,6 @@
 %{
-  "libex_undercover_nif-v0.6.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:b91fdd60ae326f5c639096ab34652a2dd9d94f4b65b87178a7d822cf3c2b47f6",
-  "libex_undercover_nif-v0.6.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:577267b80c5162911563da03bd66540c27f628cf49422f3e7b23f0cfe4db542e",
+  "libex_undercover_nif-v0.6.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:3f6c33ccb7d81bb00210156aec70af27074e92cd9a65bf39eca76bb114642666",
+  "libex_undercover_nif-v0.6.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:f86e25f2ca41f6b15ca509e4ecf28ee605c16a8ed430045b8e863db9ae370c2c",
 }
