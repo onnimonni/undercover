714263337d69fd54817e82e3270b2ba68e09239d
Onni Hakala <onni@flaky.build>
2026-04-22 07:10:08 +0300

    fix: generate_checksums rebase before push; add v0.6.0 checksums

--- checksum-Elixir.ExUndercover.Nif.exs
+++ checksum-Elixir.ExUndercover.Nif.exs
@@ -1,6 +1,6 @@
 %{
-  "libex_undercover_nif-v0.5.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:292568e37ed518acdb64de9b807161f0b9acd4283d9d003eb8fb217c2a0ba15f",
-  "libex_undercover_nif-v0.5.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:ce6cd9b79cb8541fe9d68ae9c13639d52064177b52f6f2a0160c0a99e6dfa88b",
+  "libex_undercover_nif-v0.6.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:b91fdd60ae326f5c639096ab34652a2dd9d94f4b65b87178a7d822cf3c2b47f6",
+  "libex_undercover_nif-v0.6.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:577267b80c5162911563da03bd66540c27f628cf49422f3e7b23f0cfe4db542e",
 }
